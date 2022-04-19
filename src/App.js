4412b45ebe134a767828dbdeee4416b3c7ee3f09
Thayara Siqueira <thay.siqu@gmail.com>
2022-04-19 13:40:50 -0600

    add requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Search from './components/Search';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
+import ProfileEdit from './ProfileEdit';
+import NotFound from './components/NotFound';
 
-// primeiro commit
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/" component={ NotFound } />
+      </BrowserRouter>
+    );
   }
 }
 
