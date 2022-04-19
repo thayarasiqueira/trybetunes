af8860e6df4ae25c03674c04a7c4683afc07f204
Thayara Siqueira <thay.siqu@gmail.com>
2022-04-19 16:17:47 -0600

    add requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import Login from './components/Login';
 import Search from './components/Search';
 import Album from './components/Album';
@@ -7,19 +7,74 @@ import Favorites from './components/Favorites';
 import Profile from './components/Profile';
 import ProfileEdit from './ProfileEdit';
 import NotFound from './components/NotFound';
+import { createUser } from './services/userAPI';
+import Loading from './components/Loading';
+
+const minInputName = 3;
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disabled: true,
+      name: '',
+      loading: false,
+      logged: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLoginBtn = this.handleLoginBtn.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      if (value.length < minInputName) {
+        this.setState({ disabled: true,
+        });
+      } else {
+        this.setState({ disabled: false,
+        });
+      }
+    });
+  }
+
+  handleLoginBtn() {
+    const { name } = this.state;
+    this.setState({
+      loading: true,
+    }, async () => {
+      await createUser({ name });
+      this.setState({ loading: false, logged: true });
+    });
+  }
+
   render() {
+    const { disabled, name, loading, logged } = this.state;
     return (
-      <BrowserRouter>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/search" component={ Search } />
-        <Route exact path="/album/:id" component={ Album } />
-        <Route exact path="/favorites" component={ Favorites } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/profile/edit" component={ ProfileEdit } />
-        <Route path="/" component={ NotFound } />
-      </BrowserRouter>
+      <main>
+        <BrowserRouter>
+          { loading && <Loading /> }
+          <Route
+            exact
+            path="/"
+            render={ (props) => (
+              <Login
+                { ...props }
+                disabled={ disabled }
+                name={ name }
+                handleChange={ this.handleChange }
+                handleLoginBtn={ this.handleLoginBtn }
+              />) }
+          />
+          { logged && <Redirect to="/search" /> }
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/" component={ NotFound } />
+        </BrowserRouter>
+      </main>
     );
   }
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
   render() {
+    const { disabled, name, handleChange, handleLoginBtn } = this.props;
     return (
       <div data-testid="page-login">
-        <p>Login</p>
+        <form>
+          <label htmlFor="name">
+            <input
+              data-testid="login-name-input"
+              type="text"
+              id="name"
+              name="name"
+              minLength="3"
+              placeholder="Digite seu nome"
+              value={ name }
+              onChange={ handleChange }
+            />
+          </label>
+          <button
+            data-testid="login-submit-button"
+            type="submit"
+            onClick={ handleLoginBtn }
+            disabled={ disabled }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  handleLoginBtn: PropTypes.func.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 
 export default Login;
