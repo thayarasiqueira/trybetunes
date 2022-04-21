c570a69eb8d816fe7f65cd37ff42f48a532219d8
Thayara Siqueira <thay.siqu@gmail.com>
2022-04-21 11:51:40 -0600

    corrige alteracao que fez o 2 não passar

--- src/App.js
+++ src/App.js
@@ -103,7 +103,6 @@ class App extends React.Component {
               />) }
           />
           { logged && <Redirect to="/search" /> }
-          { loading && <Loading />}
           <Route
             exact
             path="/search"
